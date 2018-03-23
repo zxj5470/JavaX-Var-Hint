d739a9475499f4c12625fb9b69eac1c85ff54b3a
zxj5470 <zxj5470@foxmail.com>
2018-03-23 21:23:05 +0800

    init commit

---
+++ src/main/kotlin/com/github/zxj5470/javaxhint/JavaXVarTypeHintFolderBuilder.kt
@@ -0,0 +1,40 @@
+package com.github.zxj5470.javaxhint
+
+import com.intellij.lang.ASTNode
+import com.intellij.lang.folding.*
+import com.intellij.openapi.editor.Document
+import com.intellij.psi.*
+
+/**
+ * @author: zxj5470
+ * @date: 2018/3/23
+ */
+class JavaXVarTypeHintFolderBuilder : FoldingBuilderEx() {
+	override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
+		val ret = ArrayList<FoldingDescriptor>()
+		root.children.filterIsInstance<PsiClass>().forEach {
+			it.children.filterIsInstance<PsiMethod>().forEach {
+				it.children.first { it is PsiCodeBlock }
+					.children.forEach {
+					findVarDeclaration(it).let { (bool, psi, text) ->
+						if (bool) {
+							ret.add(getFold(psi!!, text!!))
+						}
+					}
+				}
+			}
+		}
+		return ret.toTypedArray()
+	}
+
+	private fun getFold(elem: PsiElement, placeHolder: String) =
+		object : FoldingDescriptor(elem, elem.textRange) {
+			override fun getPlaceholderText(): String? {
+				return placeHolder
+			}
+		}
+
+	override fun getPlaceholderText(node: ASTNode) = "..."
+
+	override fun isCollapsedByDefault(node: ASTNode) = true
+}
---
+++ src/main/kotlin/com/github/zxj5470/javaxhint/utils.kt
@@ -0,0 +1,56 @@
+package com.github.zxj5470.javaxhint
+
+import com.intellij.psi.*
+
+/**
+ * @author: zxj5470
+ * @date: 2018/3/22
+ */
+fun findVarDeclaration(element: PsiElement): Triple<Boolean, PsiElement?, String?> =
+	when (element) {
+		is PsiDeclarationStatement -> {
+			var boolean = false
+			// no blank before and after typeText
+			var typeText: String? = null
+			var elem: PsiElement? = null
+			element.declaredElements.first().let {
+				it.children
+					.filter { it is PsiIdentifier || it is PsiTypeElement || it is PsiJavaCodeReferenceElement || it is PsiNewExpression || it is PsiLiteralExpression }
+					.forEach {
+						when (it) {
+							is PsiTypeElement, is PsiJavaCodeReferenceElement -> {
+								if (it.text == "var") {
+									boolean = true
+								}
+							}
+							is PsiIdentifier -> {
+								if (boolean)
+									elem = it
+							}
+							is PsiNewExpression -> {
+								typeText = it.type?.presentableText
+								if (it.children.any { it is PsiAnonymousClass }) {
+									typeText = "< anonymous extends $typeText >"
+								}
+							}
+							is PsiLiteralExpression -> {
+								typeText = it.type?.presentableText
+							}
+						}
+					}
+				if (boolean) {
+					elem?.nextSibling?.let {
+						if (it.text == "=") {
+							elem = elem?.nextSibling
+							typeText = " : $typeText ="
+						} else {
+							elem = elem?.nextSibling
+							typeText = " : $typeText "
+						}
+					}
+				}
+				Triple(boolean, elem, typeText)
+			}
+		}
+		else -> Triple(false, null, null)
+	}
