7fbbaf95d6dde5924a860c3fdf4bbdbb0e259ed9
ice1000 <ice1000@sbrella.com>
2018-03-29 12:24:36 +0800

    Minor modifications

--- src/main/kotlin/com/github/zxj5470/javaxhint/JavaXVarTypeHintFolderBuilder.kt
+++ src/main/kotlin/com/github/zxj5470/javaxhint/JavaXVarTypeHintFolderBuilder.kt
@@ -4,36 +4,26 @@ import com.intellij.lang.ASTNode
 import com.intellij.lang.folding.*
 import com.intellij.openapi.editor.Document
 import com.intellij.psi.*
+import com.intellij.psi.util.PsiTreeUtil
 
 /**
  * @author: zxj5470
  * @date: 2018/3/23
  */
 class JavaXVarTypeHintFolderBuilder : FoldingBuilderEx() {
-	override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
-		val ret = ArrayList<FoldingDescriptor>()
-		if(root.hasNoError)
-		root.children.filterIsInstance<PsiClass>().forEach {
-			it.children.filterIsInstance<PsiMethod>().forEach {
-				it.children.first { it is PsiCodeBlock }
-					.children.forEach {
+	override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> =
+		if (root.hasNoError) PsiTreeUtil.findChildrenOfType(root, PsiClass::class.java).flatMap {
+			PsiTreeUtil.findChildrenOfType(root, PsiMethod::class.java).flatMap {
+				PsiTreeUtil.findChildOfType(it, PsiCodeBlock::class.java)?.children?.mapNotNull {
 					findVarDeclaration(it).let { (bool, psi, text) ->
-						if (bool) {
-							ret.add(getFold(psi!!, text!!))
-						}
+						if (bool && psi != null) getFold(psi, text) else null
 					}
-				}
+				}.orEmpty()
 			}
-		}
-		return ret.toTypedArray()
-	}
+		}.toTypedArray() else emptyArray()
 
-	private fun getFold(elem: PsiElement, placeHolder: String) =
-		object : FoldingDescriptor(elem, elem.textRange) {
-			override fun getPlaceholderText(): String? {
-				return placeHolder
-			}
-		}
+	private fun getFold(elem: PsiElement, placeHolder: String?) =
+		NamedFoldingDescriptor(elem.node, elem.textRange, null, placeHolder.orEmpty())
 
 	override fun getPlaceholderText(node: ASTNode) = "..."
 
